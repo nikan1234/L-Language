b573da8c605f48d62c9feb1eb6525a3f359399f0
nikan1234 <nikitabrn1999@gmail.com>
2023-05-05 03:35:11 +0700

    Init logic ops

---
+++ Translator/src/main/java/ru/nsu/logic/lang/ast/ASTLogicStatements.java
@@ -0,0 +1,18 @@
+package ru.nsu.logic.lang.ast;
+
+import ru.nsu.logic.lang.common.LogicOperator;
+
+public class ASTLogicStatements {
+
+    public static class Unary extends ASTUnaryStatement<LogicOperator> {
+        public Unary(Node operand, LogicOperator operator, FileLocation location) {
+            super(operand, operator, location);
+        }
+    }
+
+    public  static class Binary extends ASTBinaryStatement<LogicOperator> {
+        public Binary(Node lhs, Node rhs, LogicOperator operator, FileLocation location) {
+            super(lhs, rhs, operator, location);
+        }
+    }
+}
---
+++ Translator/src/main/java/ru/nsu/logic/lang/common/LogicOperator.java
@@ -0,0 +1,5 @@
+package ru.nsu.logic.lang.common;
+
+public enum LogicOperator {
+    AND, OR, NOT
+}
